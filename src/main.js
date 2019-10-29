30d2e43bfdf8e21f1e55a88506d4a19bdb8ce86c
Yan <shenyou.yan@capgemini.com>
2019-10-29 10:21:19 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
+import router from "./router/router.js";
+import store from "./store/store.js";
+import { service, request } from './utils/request.js';
+Vue.prototype.$request = request;
+Vue.prototype.$axios = service;
 Vue.config.productionTip = false;
+// 手势库
+import AlloyFinger from 'alloyfinger';
+import AlloyFingerVue from 'alloyfinger/vue/alloy_finger_vue.js';
+Vue.use(AlloyFingerVue, { AlloyFinger:  AlloyFinger });
+
+// css
+import './style/base.scss';
+import './style/reset.scss';
+import './style/mixin.scss';
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+        <div
+            v-finger:tap="tap"
+            v-finger:multipoint-start="multipointStart"
+            v-finger:long-tap="longTap"
+            v-finger:swipe="swipe"
+            v-finger:pinch="pinch"
+            v-finger:rotate="rotate"
+            v-finger:press-move="pressMove.bind(this, 12)"
+            v-finger:multipoint-end="multipointEnd"
+            v-finger:double-tap="doubleTap"
+            v-finger:single-tap="singleTap"
+            v-finger:touch-start="touchStart"
+            v-finger:touch-move="touchMove"
+            v-finger:touch-end="touchEnd"
+            v-finger:touch-cancel="touchCancel"
+        >
+            <div>the element that you want to bind event</div>
+        </div>
+        <!-- <div class="main">
+
+        </div> -->
+        <div>
+            
+            <input type="file" accept="image/*" multiple="multiple"/>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,9 +35,72 @@
 import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "home",
-  components: {
-    HelloWorld
-  }
+    name: "home",
+    components: {
+        // HelloWorld
+    },
+    mounted() {},
+    methods: {
+        tap: function() {
+            // 点击
+            console.log("onTap");
+        },
+        multipointStart: function() {
+            console.log("onMultipointStart");
+        },
+        longTap: function() {
+            // 长按屏幕
+            console.log("onLongTap");
+        },
+        swipe: function(evt) {
+            console.log("swipe" + evt.direction);
+            console.log("onSwipe");
+        },
+        pinch: function(evt) {
+            console.log(evt.scale);
+            console.log("onPinch");
+        },
+        rotate: function(evt) {
+            console.log(evt.angle);
+            console.log("onRotate");
+        },
+        pressMove: function(num, evt) {
+            console.log(evt.deltaX);
+            console.log(evt.deltaY);
+            console.log("onPressMove with params:" + num);
+        },
+        multipointEnd: function() {
+            console.log("onMultipointEnd");
+        },
+        doubleTap: function() {
+            console.log("onDoubleTap");
+        },
+        singleTap: function() {
+            console.log("onSingleTap");
+        },
+
+        touchStart: function() {
+            console.log("onTouchStart");
+        },
+        touchMove: function() {
+            console.log("onTouchMove");
+        },
+        touchEnd: function() {
+            console.log("onTouchEnd");
+        },
+        touchCancel: function() {
+            console.log("onTouchCancel");
+        }
+    }
 };
 </script>
+<style scoped lang="scss">
+.main {
+    width: 6.4rem;
+    height: 3.8rem;
+    background: #000000;
+    border-radius: .1rem;
+    margin-left: .56rem;
+}
+</style>
+
